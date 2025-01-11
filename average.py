02e380f2e5b09abeaf67924a01999f6a1b356308
anupama <anupamapp310@gmail.com>
2025-01-11 23:26:13 +0530

    average

---
+++ average.py
@@ -0,0 +1,5 @@
+maths=int(input("enter maths mark:"))
+physics=int(input("enter physics mark:"))
+chemistry=int(input("enter chemistry mark:"))
+average=(maths+physics+chemistry)/3
+print('average is:',average)
---
+++ bitcoin.py
@@ -0,0 +1,6 @@
+bitcoinvalue=10000
+pincrease=10
+totalvincrease=(bitcoinvalue*pincrease)/100
+tbitcoin=bitcoinvalue+totalvincrease
+print('total value increase',totalvincrease)
+print('total bit coin value',tbitcoin)
